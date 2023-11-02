5efe2dbe92b9eee7af698974afad30c54e1ce920
Nikita Rolis <nikita.rolis1@gmail.com>
2023-11-02 22:09:06 +0300

    Removed non usable symbols as "( and )", renamed variable as ipnmask.

--- linux/nikita/hostnew.sh
+++ linux/nikita/hostnew.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ip=$(ip addr | grep -w inet | awk '/brd/{print $2}')
-nmap -sn $ip | awk '/Nmap scan report/{host=$NF} /Host is up/{time=$(NF-1)} END{print host, time}' 
+ipnmask=$(ip addr | grep -w inet | awk '/brd/{print $2}')
+nmap -sn $ipnmask | sed 's/[()]//g' | awk '/Nmap scan report/{host=$NF} /Host is up/{time=$(NF-1)} END{print host, time}'
