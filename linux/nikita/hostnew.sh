daa8699e0385d56f289252d64052cfa4aeab77c6
Nikita Rolis <nikita.rolis1@gmail.com>
2023-11-09 18:44:01 +0300

    Optimised for script, ip searching process. Now script can really well work, on different personals computers.

--- linux/nikita/hostnew.sh
+++ linux/nikita/hostnew.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ipnmask=$(ip addr | grep enp0s | grep -w inet | awk '/inet/{print $2}')
+ipnmask=$(ip addr | sed '1,7d' | grep -w 'inet' |  awk '{print $2}')
 
 host_find() {
 	nmap -sn -v $ipnmask | sed 's/[()]//g' | grep -v down | awk '/Nmap scan report/ {ip=$NF} /Host is up/ {latency=$(NF-1); print ip, latency}'
