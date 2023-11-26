50632bfeb1c505b86896d6f6ed69253b38dcc606
Rolislaff <nikita.rolis1@gmail.com>
2023-11-26 17:39:15 +0300

    fixed(host-scanning-script)

--- linux/nikita/hostnew.sh
+++ linux/nikita/hostnew.sh
@@ -1,37 +1,40 @@
 #!/bin/bash
 
-#In start my script need to make sure that you have instaled tools in linux
 command=/usr/bin/nmap
-if [ -f $command ]
-then
-	echo "Great, your pc is configured to run Host-Scanning-Script"
-		sleep 2
-			clear
-else
+ipnmask=$(ip addr | sed '1,7d' | grep -w 'inet' |  awk '{print $2}')
+
 choose_fun() {
 	echo "Bad news script cannot be completed, you want to install nmap command?"
 	read -p "Yes/No:" answer
-		if [ $answer = "No" ]; then
-		echo "This script will be closed"
-		exit
-		elif [ $answer = "Yes" ]; then
-		echo "I'm installing nmap now."
-		sudo apt install -y nmap
-		clear
-		echo "Congratulation nmap command is installed"
-		sleep 3
-		clear
+if [ $answer = "No" ]; then
+	echo "This script will be closed"
+	exit
+elif [ $answer = "Yes" ]; then
+	echo "I'm installing nmap now."
+	version_fun
+	echo "Congratulation nmap command is installed"
+	host_find_fun
 fi
 }
-choose_fun
-fi
 
-#Variable which taking ip-address and net_mask
-ipnmask=$(ip addr | sed '1,7d' | grep -w 'inet' |  awk '{print $2}')
-
-#Function which scanning your local network for avaible hosts, and returning clear output with ip-addres and latency
-host_find() {
+host_find_fun() {
+	sleep 3
+	clear
         nmap -sn -v $ipnmask | sed 's/[()]//g' | grep -v down | awk '/Nmap scan report/ {ip=$NF} /Host is up/ {latency=$(NF-1); print ip, latency}'
 }
 
-host_find
+version_fun() {
+	sudo apt install -y nmap > /dev/null 2>/dev/null
+if [ $? == 1 ]; then
+	echo "Unexpected problem your based version is not suppost to download nmap command, please do it manually"
+	exit
+fi
+}
+
+if [ -f $command ]
+then
+	echo "Great, your pc is configured to run Host-Scanning-Scipt"
+	host_find_fun
+else
+	choose_fun
+fi
