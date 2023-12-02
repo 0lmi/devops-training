2dfa448afbdc852d298a569c723781a58dd1c6f1
Tymofii <timofeychernata@gmail.com>
2023-12-02 19:40:53 +0100

    Docker task

--- linux/tima/docker.sh
+++ linux/tima/docker.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-docker stop donotest
-docker rm donotest
-docker run -d --name donotest -p 80:80 0lmi/donotest
+docker stop donotest >/dev/null 2>/dev/null
+docker rm donotest >/dev/null 2>/dev/null
+docker pull 0lmi/donotest >/dev/null 2>/dev/null
+docker run -d --name donotest -p 80:80 0lmi/donotest >/dev/null
+start=$(date +%s.%N)
 
-n=0
 while true; do
-    let n=$n+1
     curl http://127.0.0.1 >/dev/null 2>/dev/null
     if [ $? -eq 0 ]; then
+		end=$(date +%s.%N)
         break
     fi
 done
-echo $n
+
+
+dur=$(echo "$end - $start" | bc)
+LC_NUMERIC="en_US.UTF-8" printf "Execution time: %.6f seconds\n" $dur
 
