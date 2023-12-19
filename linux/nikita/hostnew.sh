2698d7406d89ae60976a15535dddf1e2c3292cf7
Rolislaff <nikita.rolis1@gmail.com>
2023-12-19 18:18:24 +0300

    Host scanning script

--- linux/nikita/hostnew.sh
+++ linux/nikita/hostnew.sh
@@ -6,27 +6,28 @@ ipnmask=$(ip addr | sed '1,7d' | grep -w 'inet' |  awk '{print $2}')
 choose_fun() {
     echo "Bad news script cannot be completed, you want to install nmap command?"
     read -p "Yes/No:" answer
-        if [ $answer = "No" ] then;
-            echo "This script will be closed"
-            exit
-        elif [ $answer = "Yes" ] then;
-            echo "I'm installing nmap now"
-            version_fun
-            echo "Congratulation nmap command is installed"
-            host_find_fun
-        fi
+    if [ $answer = "No" ]; then
+        echo "This script will be closed"
+        exit
+    elif [ $answer = "Yes" ]; then
+        echo "I'm installing nmap now"
+        version_fun
+        echo "Congratulation nmap command is installed"
+        host_find_fun
+    fi
 }
 
 host_find_fun() {
-    nmap -sn -v $ipnmask | sed 's/[()]//g' | grep -v down | awk '/Nmap scan report/ {ip=$NF} /Host is up/ {latency=$(NF-1); print ip, latency}'
+    nmap -sn -v $ipnmask | sed 's/[()]//g' | grep -v down | \
+        awk '/Nmap scan report/ {ip=$NF} /Host is up/ {latency=$(NF-1); print ip, latency}'
 }
 
 version_fun() {
     sudo apt install -y nmap > /dev/null 2>/dev/null
-        if [ $? == 1 ] then;
-            echo "Unexpected problem your based version is not suppost to download nmap command, please do it manually"
-            exit
-        fi
+    if [ $? == 1 ]; then
+        echo "Unexpected problem your based version is not suppost to download nmap command, please do it manually"
+        exit
+    fi
 }
 
 if [ -f $command ]; then
